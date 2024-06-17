5de4a33d321f1dc51e4bcfb533f4b1d2bf6dfa01
Facundo Gerstner <facugerstner_29@hotmail.com>
2024-06-17 12:44:22 -0300

    working

--- code-reviewer/tools.py
+++ code-reviewer/tools.py
@@ -12,28 +12,31 @@ load_dotenv()
 def create_comment(comment: str, filename: str, line: int):
     """Creates a comment in the current pull request in the specified file (filename) and on the specified line."""
 
-    print('comment: ')
-    print(comment)
+    pr = get_pull_request()
+    last_commit = pr.get_commits()[pr.commits - 1]
+    pr.create_comment(comment, last_commit, filename, line)
 
 @tool
 def is_test(filename):
     """Returns true if the current file is a test file, false otherwhise"""
     return "spec" in filename or "test" in filename
 
-def get_changed_files():
-    """Returns a list of dicts, where each element in the list has the following format:
-    {
-        filename: name of the file
-        file: file contents,
-        changes: changes made
-    }
+@tool
+def find_line(code_extract: bytes, filename: str):
+    """Finds the line number of the given code_extract as bytes in the filename
+    Example parameters: code_extreact: b'console.log("debug")', filename: 'src/Desktop.tsxt'
     """
+    file_content = get_file(filename)
+    lines = file_content.split(b'\n')
+    for i, line in enumerate(lines):
+        if code_extract in line:
+            return i + 1
 
+def get_changed_files():
     pr = get_pull_request()
     return [
         {
             "filename": file.filename,
-            "file": get_file(file.filename),
             "changes": file.patch
         }
         for file in pr.get_files()
@@ -42,7 +45,7 @@ def get_changed_files():
 def get_file(filename):
     repo = get_repo()
     pr = get_pull_request()
-    return base64.b64decode(repo.get_contents(filename, pr.base.ref).raw_data.get('content'))
+    return base64.b64decode(repo.get_contents(filename, pr.head.ref).raw_data.get('content'))
 
 def get_pull_request():
     return get_repo().get_pull(int(os.getenv('PR_NUMBER')))
