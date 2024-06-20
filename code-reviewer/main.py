693f61ae9736504de5a0374bcf705ae7b698e127
Facundo Gerstner <facugerstner_29@hotmail.com>
2024-06-20 10:20:41 -0300

    improve prompt

--- code-reviewer/main.py
+++ code-reviewer/main.py
@@ -19,14 +19,13 @@ prompt = ChatPromptTemplate.from_messages(
                 When asked to review a list of files, for each file, you should do these steps in order 
                 1. Determine if it is a test file. If it isn't skip it
                 2. Find all errors in the given changes, ignore existing errors and focus on the newones.
+                   Any test that does not follow these guidelines should be considered as an error:
+                    a. Tests should not start with 'should'. They should be written in third person in the present tense. For example, it('should render...') must be fixed to it('renders...').
+                    b. Tests should have semantic meaning with the 'it' function provided by Jest. For example, it('make...') must be fixed to it('makes...').
+                    c. If there is no tests for the edge cases, testing will not be useful. Tests should cover valid, edge and invalid cases. For example, if there is a context: describe('when request succeeds'), then there should be another context: describe('when request fails'). you should indicate the missing scenarios if there are any. 
+                    d. There should be no typos in tests names
                 3. Find the line number for each of the errors found on the previous step
-                4. Create a comment in spanish for each error in the current pull request
-
-                Tests should follow these guidelines:
-                a. Each test starts with 'it' or 'describe' functions. Make a detailed review for each file
-                b. Tests should not start with 'should'. For example, it('should render...') must be fixed to it('renders...').
-                c. Tests should have semantic meaning with the 'it' function provided by Jest. For example, it('make...') must be fixed to it('makes...').
-                d. If any test states a given success scenario, there should be another test with the error scenario. For example, if there is a context: describe('when request succeeds'), then there should be another context: describe('when request fails'). you should indicate the missing scenarios if there are any.
+                4. Create a comment for each error in the current pull request
             """,
         ),
         ("user", "Review this list of files: {files}"),
